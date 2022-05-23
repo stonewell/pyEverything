aaf9101d0dad1ada3ad6b33850b23935b487b013
Jingnan Si <jingnan.si@gmail.com>
2022-05-23 16:26:47 -0700

    add setup files

--- src/web/run_everything.py
+++ src/web/run_everything.py
@@ -1,7 +1,10 @@
 from . import create_app
 from .service import start_indexer
 
-if __name__ == '__main__':
+def main():
   start_indexer()
 
   create_app().run(debug=True, port=8192)
+
+if __name__ == '__main__':
+  main()
